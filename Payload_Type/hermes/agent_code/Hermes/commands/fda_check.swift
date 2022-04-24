695258ba33f86614412e44a1b4f96bb581b9af67
Justin Bui <slyd0g@Justins-MBP.frontiernet.net>
2022-04-23 22:38:10 -0700

    fda_check updated to add mdquery technique

--- Payload_Type/hermes/agent_code/Hermes/commands/fda_check.swift
+++ Payload_Type/hermes/agent_code/Hermes/commands/fda_check.swift
@@ -9,15 +9,52 @@ import Foundation
 
 func fullDiskAccessCheck(job: Job) {
     do {
-        var path = "~/Library/Application Support/com.apple.TCC/TCC.db"
-        path = NSString(string: path).expandingTildeInPath
-        _ = try Data(contentsOf: URL(fileURLWithPath: path))
+        // Convert json to technique
+        let jsonParameters = try JSON(data: toData(string: job.parameters))
+        let technique = jsonParameters["technique"].stringValue
         
-        // Set to true for future ls calls, will allow to read extended attributes
-        tccFullDiskAccess = true
-        job.result = "\"Full Disk Access\" is enabled!"
-        job.completed = true
-        job.success = true
+        if technique == "file handle" {
+            var path = "~/Library/Application Support/com.apple.TCC/TCC.db"
+            path = NSString(string: path).expandingTildeInPath
+            _ = try Data(contentsOf: URL(fileURLWithPath: path))
+            
+            // Set to true for future ls calls, will allow to read extended attributes
+            tccFullDiskAccess = true
+            job.result = "\"Full Disk Access\" is enabled!"
+            job.completed = true
+            job.success = true
+        }
+        else if technique == "mdquery" {
+            var pathFound = false
+            let queryString = "kMDItemDisplayName = *TCC.db"
+            let username = NSUserName()
+            if let query = MDQueryCreate(kCFAllocatorDefault, queryString as CFString, nil, nil) {
+                MDQueryExecute(query, CFOptionFlags(kMDQuerySynchronous.rawValue))
+                for i in 0..<MDQueryGetResultCount(query) {
+                    if let rawPtr = MDQueryGetResultAtIndex(query, i) {
+                        let item = Unmanaged<MDItem>.fromOpaque(rawPtr).takeUnretainedValue()
+                        if let path = MDItemCopyAttribute(item, kMDItemPath) as? String {
+                            if path.hasSuffix("/Users/\(username)/Library/Application Support/com.apple.TCC/TCC.db"){
+                                pathFound = true
+                            }
+                        }
+                    }
+                }
+                if pathFound {
+                    // Set to true for future ls calls, will allow to read extended attributes
+                    tccFullDiskAccess = true
+                    job.result = "\"Full Disk Access\" is enabled!"
+                    job.completed = true
+                    job.success = true
+                }
+                else {
+                    job.result = "Error: \"Full Disk Access\" has not been granted"
+                    job.completed = true
+                    job.success = false
+                    job.status = "error"
+                }
+            }
+        }
     }
     catch {
         job.result = "Error: \"Full Disk Access\" has not been granted"
--- Payload_Type/hermes/mythic/agent_functions/fda_check.py
+++ Payload_Type/hermes/mythic/agent_functions/fda_check.py
@@ -5,9 +5,25 @@ import json
 class FullDiskAccessCheckArguments(TaskArguments):
     def __init__(self, command_line, **kwargs):
         super().__init__(command_line, **kwargs)
-        self.args = []
+        self.args = [
+            CommandParameter(
+                name="technique",
+                type=ParameterType.ChooseOne,
+                description="Full Disk Access enumeration technique",
+                choices=[
+                    "file handle",
+                    "mdquery",
+                ],
+                default_value="mdquery",
+                parameter_group_info=[ParameterGroupInfo(
+                    required=True
+                )]
+            ),
+        ]
 
     async def parse_arguments(self):
+        if len(self.command_line) == 0:
+            raise Exception("Must supply an enumeration technique")
         pass
 
 
@@ -15,7 +31,7 @@ class FullDiskAccessCheckCommand(CommandBase):
     cmd = "fda_check"
     needs_admin = False
     help_cmd = "fda_check"
-    description = "Attempts to access ~/Library/Application\ Support/com.apple.TCC/TCC.db to determine if you have \"Full Disk Access\" permissions. Technique inspired by @cedowens who also implemented this check in https://github.com/cedowens/SwiftBelt-JXA"
+    description = "1. Attempts to access ~/Library/Application\ Support/com.apple.TCC/TCC.db to determine if you have \"Full Disk Access\" permissions.\n2. Uses mdquery API calls to determine if you have \"Full Disk Access\" permissions.\nTechnique inspired by @cedowens who also implemented this check in https://github.com/cedowens/SwiftBelt-JXA and https://github.com/cedowens/Spotlight-Enum-Kit"
     version = 1
     author = "@slyd0g"
     argument_class = FullDiskAccessCheckArguments
